89078b5643c91da08a6906eb1ac1fec209453248
LukaszPB <lukasz.kiersnowski.110752@student.pb.edu.pl>
2024-05-19 22:48:22 +0200

    Initial commit

---
+++ src/main/java/com/example/green_assets/GreenAssetsApplication.java
@@ -0,0 +1,13 @@
+package com.example.green_assets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenAssetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenAssetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/green_assets/GreenAssetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.green_assets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenAssetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
